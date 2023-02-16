082fe7962d63a3ae3f272f7af6da9366d2e70728
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-02-16 21:43:27 +0300

    fix: исправить верстку компонентов

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -10,9 +10,9 @@ export default function Promo() {
         <div className='promo__left-side'>
           <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
           <p className='promo__text'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-          <button className='button promo__button'></button>
+          <button className='button promo__button'>Узнать больше</button>
         </div>
-        <div className='promo_logo'></div>
+        <div className='promo__logo'></div>
       </div>
     </section>
   )
--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -2,33 +2,35 @@ import './Techs.scss';
 
 export default function Techs() {
   return (
-    <section className='techs section'>
-      <h2 className='techs__title section__header'>Технологии</h2>
-      <h3 className='tesh__subtitle'>7 технологий</h3>
-      <p className='tech__text'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
-      <ul className='tech__list'>
-        <li className='tech__cell'>
-          <p className='tech__text-cell'>HTML</p>
-        </li>
-        <li className='tech__cell'>
-          <p className='tech__text-cell'>CSS</p>
-        </li>
-        <li className='tech__cell'>
-          <p className='tech__text-cell'>JS</p>
-        </li>
-        <li className='tech__cell'>
-          <p className='tech__text-cell'>React</p>
-        </li>
-        <li className='tech__cell'>
-          <p className='tech__text-cell'>Git</p>
-        </li>
-        <li className='tech__cell'>
-          <p className='tech__text-cell'>Express.js</p>
-        </li>
-        <li className='tech__cell'>
-          <p className='tech__text-cell'>mongoDB</p>
-        </li>
-      </ul>
+    <section className='techs'>
+      <h2 className='techs__title page__title'>Технологии</h2>
+      <div className='techs__container container'>
+        <h3 className='techs__subtitle'>7 технологий</h3>
+        <p className='techs__text'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+        <ul className='techs__list'>
+          <li className='techs__cell'>
+            <p className='techs__text-cell'>HTML</p>
+          </li>
+          <li className='techs__cell'>
+            <p className='techs__text-cell'>CSS</p>
+          </li>
+          <li className='techs__cell'>
+            <p className='techs__text-cell'>JS</p>
+          </li>
+          <li className='techs__cell'>
+            <p className='techs__text-cell'>React</p>
+          </li>
+          <li className='techs__cell'>
+            <p className='techs__text-cell'>Git</p>
+          </li>
+          <li className='techs__cell'>
+            <p className='techs__text-cell'>Express.js</p>
+          </li>
+          <li className='techs__cell'>
+            <p className='techs__text-cell'>mongoDB</p>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
