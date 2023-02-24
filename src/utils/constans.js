9713bd720f684f2b8d812c93a1d0bcb5dbd89788
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-02-24 15:24:01 +0300

    feat: добавить стейт кнопке лайка

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -1,7 +1,10 @@
 const cards = [
-
-
-
+  {
+    _id: 12,
+    name: 'Киноальманах «100 лет дизайна»',
+    link: 'https://i.pinimg.com/736x/dc/55/db/dc55db0b2acde4a52e0d81c835110eb2.jpg',
+    time: '1ч 42м'
+  }
 ];
 
 export {
