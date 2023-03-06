f946d327102224f69e888b4e2c9edb7f16e1caf0
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-03-06 10:31:09 +0300

    fix: исправить навигацию

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,20 +6,20 @@ export default function Portfolio() {
       <h3 className='portfolio__title'>Портфолио</h3>
       <ul className='portfolio__list'>
         <li className='portfolio__cell'>
-          <h4 className='portfolio__name'>Статический сайт</h4>
           <a className='link portfolio__link' href='https://github.com/DocMoro'>
+            <h4 className='portfolio__name'>Статический сайт</h4>
             <div className='portfolio__image'></div>
           </a>
         </li>
         <li className='portfolio__cell'>
-          <h4 className='portfolio__name'>Адаптивный сайт</h4>
           <a className='link portfolio__link' href='https://github.com/DocMoro'>
+            <h4 className='portfolio__name'>Адаптивный сайт</h4>
             <div className='portfolio__image'></div>
           </a>
         </li>
         <li className='portfolio__cell'>
-          <h4 className='portfolio__name'>Одностраничное приложение</h4>
           <a className='link portfolio__link' href='https://github.com/DocMoro'>
+            <h4 className='portfolio__name'>Одностраничное приложение</h4>
             <div className='portfolio__image'></div>
           </a>
         </li>
